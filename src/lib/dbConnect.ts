f2b994636049b236195d212ef1e7c4bce0464963
“Shashank <shashanktyagi12345@gmail.com>
2024-08-17 11:53:15 +0530

    chore: Update .gitignore and package.json, add resend.ts and sendVerificationEmail.ts

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+
+type ConnectionObject = {
+    isConnected? : number;
+}
+
+
+const connection : ConnectionObject = {};
+
+async function dbConnect() : Promise<void>
+{
+    try{
+    if(connection.isConnected)                                 // to prevent db chocking. If already connected, then return
+    {
+        console.log("Already connected to the database");
+        return;
+    }
+
+    const db = await mongoose.connect(process.env.MONGO_URI || "", {});
+    connection.isConnected = db.connections[0].readyState;                    
+    console.log("Connected to the database");
+}
+catch(err)
+{
+    console.log("Database connection failed");
+    console.log(err);
+    process.exit(1);
+}
+}
