29d4cdf4a8302dc017773e091c3cd06d6ff749e8
“Shashank <shashanktyagi12345@gmail.com>
2024-08-17 15:07:30 +0530

    chore: Add sendVerificationEmail helper function
    ```text
    Add sendVerificationEmail helper function to handle sending verification emails to users. The function takes in the user's email, username, and verification code, and sends an email with a verification link. If the email is sent successfully, the function returns a success message. If there is an error sending the email, the function returns a failure message.

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -1,2 +1,28 @@
 import { resend } from "@/lib/resend";
 
+import VerificationEmail from "../../emails/VerificationEmail";
+
+import { ApiResponse} from "@/types/ApiResponse";
+
+
+export async function sendVerificationEmail(email : string, username : string, verifyCode : string) : Promise<ApiResponse>
+{
+    try
+    {
+        await resend.emails.send({
+            from : "onboarding@resend.dev>",
+            to : email,
+            subject : "Verify your email for AnonSend",
+            react : VerificationEmail({username, otp : verifyCode}),
+        });
+
+        return {success : true, message : "Email sent"};
+        
+    }
+    catch(err)
+    {
+        console.log( "error sending email" , err);
+        return {success : false, message : "Falied to send email"};
+    }
+
+}
