013d3281af693f6e1513e6ebb04e1c52dbe249fc
“Shashank <shashanktyagi12345@gmail.com>
2024-08-23 21:59:29 +0530

    feat: Add accept-messages route for accepting and retrieving user messages
    The code changes include adding a new route for accepting and retrieving user messages. This route handles both POST and GET requests. For POST requests, it checks if the user is authenticated and updates the user's acceptMessages status in the database. For GET requests, it retrieves the user's acceptMessages status from the database. This route is part of the authentication and messaging functionality of the application.
    
    Closes #789

--- src/app/api/accept-messages/route.ts
+++ src/app/api/accept-messages/route.ts
@@ -4,4 +4,90 @@ import { AuthOptions } from "next-auth";
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/user";
 import {User} from  "next-auth";                          // this is not the user we stored in session before. it comes with session in next auth
+import { authOptions } from "../auth/[...nextauth]/options";
 
+export async function POST(request : Request){
+    dbConnect();
+    const session = await getServerSession(authOptions);
+
+    const user : User = session?.user;                          //  : User is asseriting the type of session.user to User type
+
+    if(!session || !session.user){
+        return Response.json({
+            success : false,
+            message : "not authenticated"
+        }, {status : 401});
+    }
+
+    const userId = user.id;
+    const {acceptMessages} = await request.json();
+
+    try{
+        const updatedUser = await UserModel.findOneAndUpdate({_id : userId}, {acceptMessages : acceptMessages}, {new : true});           // new = true will return the updated user
+        if(!updatedUser){
+            return Response.json({
+                success : false,
+                message : "failed to update user status to accept messages"
+            }, {status : 401});
+        }
+        else{
+            return Response.json({
+                success : true,
+                message : "User status updated successfully",
+                user : updatedUser
+            }, {status : 200});
+        }
+    }
+    catch(err){
+        console.log("failed to update user status to accept messages");
+        console.log(err);
+        return Response.json({
+            success : false,
+            message : "failed to update user status to accept messages",
+        }, {status : 500});
+    }
+}
+
+
+export async function GET(request : Request){
+    dbConnect();
+
+    const session = await getServerSession(authOptions);
+    const user : User = session?.user;
+    try{
+    if(!session || !session.user){
+        return Response.json({
+            success : false,
+            message : "not authenticated"
+        }, {status : 401});
+    }
+
+    const userId = user.id;
+
+    const userfromdb = await UserModel.findOne({_id : userId});
+    if(!userfromdb){
+        return Response.json({
+            success : false,
+            message : "User not found or not accepting messages"
+        }, {status : 404});
+    }
+
+    if(userfromdb.isAcceptingMessages){
+        return Response.json({
+            success : true,
+            message : "User is accepting messages",
+            user : userfromdb
+        }, {status : 200});
+    }
+
+}
+catch(err){
+    console.log("error getting user status to accept messages");
+    console.log(err);
+    return Response.json({
+        success : false,
+        message : "Error getting user status to accept messages",
+    }, {status : 500});
+}
+
+}
